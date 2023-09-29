9e74d6925607331b61e0a09eeeecd903d446886d
Gulácsi András <guland@protonmail.com>
2023-09-29 23:38:59 +0200

    update: WooCommerce templates to be compatible with version 8.1.1

--- wordpress/web/app/themes/decimus/woocommerce/checkout/payment.php
+++ wordpress/web/app/themes/decimus/woocommerce/checkout/payment.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 7.8.0
+ * @version 8.1.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -32,20 +32,19 @@ if ( ! wp_doing_ajax() ) {
                         }
                     } else {
                         echo '<div>';
-                        wc_print_notice( apply_filters('woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ), 'notice' ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+                        wc_print_notice( apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ), 'notice' ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
                         echo '</div>';
                     }
                     ?>
                 </div>
             <?php endif; ?>
-            <div class="place-order pt-3">
+            <div class="form-row place-order pt-3">
                 <noscript>
                     <?php
                     /* translators: $1 and $2 opening and closing emphasis tags respectively */
                     printf( esc_html__( 'Since your browser does not support JavaScript, or it is disabled, please ensure you click the %1$sUpdate Totals%2$s button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce' ), '<em>', '</em>' );
                     ?>
-                    <br/>
-                    <button type="submit" class="button alt" name="woocommerce_checkout_update_totals"
+                    <br/><button type="submit" class="button alt" name="woocommerce_checkout_update_totals"
                             value="<?php esc_attr_e( 'Update totals', 'woocommerce' ); ?>"><?php esc_html_e( 'Update totals', 'woocommerce' ); ?></button>
                 </noscript>
 
